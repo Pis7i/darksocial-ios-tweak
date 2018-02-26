38051c1de78cbc3dd99188760509d09c7a1db192
Pis7i <pis7iix@gmail.com>
2018-02-26 03:04:28 +0200

    Many changes
    Current position: Doesn’t work.

--- Tweak.xm
+++ Tweak.xm
@@ -1,6 +1,8 @@
 #import "SpringBoard/SpringBoard.h"
 #import "igHeaders.h"
 #import "basicHeaders.h"
+#import "UIKit/UIView.h"
+#import "UIKit/UIKit.h"
 
 %hook SpringBoard
 
@@ -15,8 +17,16 @@
 
 %hook IGMainAppHeaderView
 	
-	- (void)setBackgroundColor:(id)bgColor{
-		bgColor = [UIColor colorWithRed:0.00 green:0.00 blue:0.00 alpha:1.0];
-	}
+%end
+
+%hook IGDirectInboxCellButtonConfig
+
+- (id)initWithPrimaryText:(id)arg1 primaryTextColor:(id)arg2 primaryBackgroundColor:(id)arg3 destructiveText:(id)arg4 destructiveTextColor:(id)arg5 destructiveBackgroundColor:(id)arg6 {
+	arg2 = [UIColor colorWithRed:0.00 green:0.38 blue:1.00 alpha:1.0];
+
+		return %orig(arg1, arg2, arg3, arg4, arg5, arg6);
+		NSLog(@"twst");
+
+}
 
 %end
--- igHeaders.h
+++ igHeaders.h
@@ -1,8 +1,39 @@
-@interface IGMainAppHeaderView{
+@interface IGMainAppHeaderView : UIView {
 	UIColor *_backgroundColor;
 	UIView *_contentContainerView;
 }
 
+- (void)setBackgroundColor:(id)arg1;
+- (void)_setBackgroundColor:(id)arg1;
 - (id)delegate;
+- (void)logoButtonTapped;
 
 @end
+
+@interface IGColors
+
++ (id)navigationBarBackgroundColor;
+
+@end
+
+@interface IGDirectInboxCellButtonConfig : NSObject
+{
+    NSString *_primaryText;
+    UIColor *_primaryTextColor;
+    UIColor *_primaryBackgroundColor;
+    NSString *_destructiveText;
+    UIColor *_destructiveTextColor;
+    UIColor *_destructiveBackgroundColor;
+}
+
+@property(readonly, nonatomic) UIColor *destructiveBackgroundColor; // @synthesize destructiveBackgroundColor=_destructiveBackgroundColor;
+@property(readonly, nonatomic) NSString *destructiveText; // @synthesize destructiveText=_destructiveText;
+@property(readonly, nonatomic) UIColor *destructiveTextColor; // @synthesize destructiveTextColor=_destructiveTextColor;
+- (id)initWithPrimaryText:(id)arg1 destructiveText:(id)arg2;
+- (id)initWithPrimaryText:(id)arg1 primaryTextColor:(id)arg2 primaryBackgroundColor:(id)arg3 destructiveText:(id)arg4 destructiveTextColor:(id)arg5 destructiveBackgroundColor:(id)arg6;
+@property(readonly, nonatomic) UIColor *primaryBackgroundColor; // @synthesize primaryBackgroundColor=_primaryBackgroundColor;
+@property(readonly, nonatomic) NSString *primaryText; // @synthesize primaryText=_primaryText;
+@property(readonly, nonatomic) UIColor *primaryTextColor; // @synthesize primaryTextColor=_primaryTextColor;
+
+@end
+
